6a991b6fe427ca643e5b1121dbe069f1d4bff17e
Abrar <abrarmurad00@gmail.com>
2025-02-10 03:37:06 -0500

    code documentation client.py

--- Client.py
+++ Client.py
@@ -5,24 +5,32 @@ PORT = 65432
 
 def start_client():
   try:
+    # TCP socket
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # connect to the server
     clientSocket.connect((HOST, PORT))
     
     print("Connection establish. Type 'exit' for disconnection")
 
     while True:
+      # get user input
       message = input("> ")
+      # send message to server
       clientSocket.send(message.encode("utf-8"))
+      # exit condition
       if message.lower() == "exit":
         break
-
+      
+      # receive response from server
       received = clientSocket.recv(1024).decode("utf-8")
       print(f"Server: {received}")
 
   except Exception as e:
+    # print error messages
     print(f"[ERROR] {e}")
 
   finally:
+    # close client socket
     clientSocket.close()
 
 if __name__ == "__main__":
